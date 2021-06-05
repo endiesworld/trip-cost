296b1d20c72c165bd793e4974fabe3fbc6a7679a
endiesworld <endiesworld@gmail.com>
2021-06-05 12:10:38 +0100

    feat: updated server

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ let db, trips, expenses ;
 
 // Create a new MongoClient
 const client = new MongoClient(myCluster_uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 
@@ -21,9 +21,9 @@ async function run() {
     trips = db.collection("trips") ;
     expenses = db.collection("expenses")
     console.log("Connected successfully to server");
-  } catch(error){
-      throw new error ;
-  }
+    } catch(error){
+        throw new error ;
+    }
 }
 run().catch(console.dir);
 
@@ -31,33 +31,56 @@ const app = express() ;
 app.use(express.json())
 
 app.post("/trip", (req, res) => {
-  const name = req.body.name
-  trips.insertOne({ name: name }, (err, result) => {
-    if (err) {
-      console.error(err)
-      res.status(500).json({ err: err })
-      return
-    }
-    console.log(result)
-    res.status(200).json({ ok: true })
-  })
+    const name = req.body.name
+    trips.insertOne({ name: name }, (err, result) => {
+        if (err) {
+        console.error(err)
+        res.status(500).json({ err: err })
+        return
+        }
+        console.log(result)
+        res.status(200).json({ ok: true })
+    })
 }) ;
 
 app.get("/trips", (req, res) => {
-  trips.find().toArray((err, items) => {
-    if (err) {
-      console.error(err)
-      res.status(500).json({ err: err })
-      return
-    }
-    res.status(200).json({ trips: items })
-  })
+    trips.find().toArray((err, items) => {
+        if (err) {
+        console.error(err)
+        res.status(500).json({ err: err })
+        return
+        }
+        res.status(200).json({ trips: items })
+    })
 })
 app.post("/expense", (req, res) => {
-  /* */
+    expenses.insertOne(
+    {
+        trip: req.body.trip,
+        date: req.body.date,
+        amount: req.body.amount,
+        category: req.body.category,
+        description: req.body.description,
+    },
+    (err, result) => {
+    if (err) {
+        console.error(err)
+        res.status(500).json({ err: err })
+        return
+    }
+    res.status(200).json({ ok: true })
+    }
+    )
 })
 app.get("/expenses", (req, res) => {
-  /* */
+    expenses.find({ trip: req.body.trip }).toArray((err, items) => {
+        if (err) {
+        console.error(err)
+        res.status(500).json({ err: err })
+        return
+        }
+        res.status(200).json({ expenses: items })
+    })
 })
 
 app.listen(3000, () => console.log("Server ready"))
